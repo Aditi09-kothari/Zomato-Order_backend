328f96fec01cc9de1222fe79c8d890dafff8cdac
Aditi09-Kothari <aditi.kotharti2019@gmail.com>
2023-12-30 12:31:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const connection = require('./src/connector');
+
+// Use Express middleware for parsing URL-encoded and JSON data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/api/orders', async (req, res) => {
+  try {
+    let { offset, limit } = req.query;
+
+    // Validate and set default values for offset and limit
+    offset = Number.isInteger(+offset) && +offset >= 0 ? +offset : 0;
+    limit = Number.isInteger(+limit) && +limit > 0 ? +limit : 10;
+
+    const [data] = await connection.query('SELECT * FROM orders LIMIT ? OFFSET ?', [+limit, +offset]);
+    const [totalPageData] = await connection.query('SELECT COUNT(*) AS count FROM orders');
+
+    // Send JSON response
+    res.status(200).json({
+      status: 200,
+      data: data,
+      totalRecords: totalPageData[0].count,
+      
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 500,
+      error: 'Internal Server Error'
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
+
+module.exports = app;
---
+++ src/connector.js
@@ -0,0 +1,27 @@
+
+var mysql = require('mysql2');
+
+
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "a",
+//     database: "test",
+//     multipleStatements: true
+// });
+
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "aditi123",
+    database: "test",
+    multipleStatements: true
+});
+
+
+pool.getConnection((err, conn) =>{
+    if(err) console.log(err)
+    console.log("connection establish with Database!!!")
+})
+
+module.exports = pool.promise()
