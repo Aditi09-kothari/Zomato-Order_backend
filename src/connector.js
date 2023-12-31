aa69c7d583ac0475c5de45fbf3bb6a72f2f74554
Aditi09-Kothari <aditi.kotharti2019@gmail.com>
2023-12-31 13:24:10 +0530

    correct connection

--- src/connector.js
+++ src/connector.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Load environment variables from .env file
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "aditi123",
-    database: "test",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     multipleStatements: true
 });
 
 // Get a connection from the pool
 pool.getConnection((err, conn) => {
     if (err) {
-        console.error(err);
+        console.error("Error connecting to MySQL:", err.message);
         return;
     }
 
