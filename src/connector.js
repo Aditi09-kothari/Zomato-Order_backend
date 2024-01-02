97481b976887775d101bebb1fd1e8a6212d4e360
Aditi09-kothari <147057560+Aditi09-kothari@users.noreply.github.com>
2024-01-02 14:29:59 +0530

    Update connector.js

--- src/connector.js
+++ src/connector.js
@@ -2,10 +2,14 @@ const mysql = require('mysql2');
 require('dotenv').config(); // Load environment variables from .env file
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE,
+    host:"localhost" ,
+    user:"root" ,
+    password:"aditi123" ,
+    database: "test",
     multipleStatements: true
 });
 
